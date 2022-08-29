55fc6fe6282d437cf2e9f5578a55e5a3a52397b8
Rohan Ijare <rohanijare164@gmail.com>
2022-08-30 00:49:24 +0200

    added file

--- app/main.py
+++ app/main.py
@@ -27,3 +27,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+@app.get("/")
+def root():
+    return {'message': 'Hello welcome to my API'}
